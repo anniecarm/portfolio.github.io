f9e693fcd0276aa0ad886ca386831e490c9c3816
Annie Carmichael <42788203+anniecarm@users.noreply.github.com>
2018-11-20 22:37:38 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,22 @@
+document.addEventListener('DOMContentLoaded', function() {
+    var elems = document.querySelectorAll('.materialboxed');
+    var instances = M.Materialbox.init(elems);
+  });
+
+ $(document).ready(function(){
+    $('.materialboxed').materialbox();
+  });
+
+ document.addEventListener('DOMContentLoaded', function() {
+    var elems = document.querySelectorAll('.tap-target');
+    var instances = M.TapTarget.init(elems);
+    instance.next();
+  instance.next(3);
+   instance.close();
+  });
+
+  // Or with jQuery
+
+  $(document).ready(function(){
+    $('.tap-target').tapTarget();
+  });
